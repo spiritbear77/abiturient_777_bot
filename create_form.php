81a4b212dd4ab82fa2562586cd3b83e25bdd868b
spiritbear77 <dianasalnikova380@gmail.com>
2025-05-17 10:42:31 +0700

    Update create_form.php

--- create_form.php
+++ create_form.php
@@ -1,8 +1,1 @@
-<link rel="stylesheet" type="text/css" href="style.css">
-<form method="post" action="create.php">
-    <span>Ответ</span><br>
-    <input type="text" name="answer" required=""><br><br>
-    <span>Ключевые слова</span><br>
-    <input type="text" name="keywords"><br><br>
-    <input class="btn" type="submit" value="Добавить">
-</form>
+
