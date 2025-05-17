f28122b9e38aea06c9b6c625fb8bf23b2afec520
spiritbear77 <dianasalnikova380@gmail.com>
2025-05-17 10:45:08 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,1 @@
-<?php
-    try{
-        //$q=new PDO('mysql:dbname=baze41;host=localhost;port=3306','root','');
-        $q = new PDO('mysql:dbname=abd41-4;host=80.87.194.140;port=3306', 'abd41', 'abd41abd41');
-    } catch(PDOException $e){
-        die($e->getMessage());
-    }
-    $r=$q->prepare('DELETE FROM `answers` WHERE `id`=:id');
-    $r->execute(['id'=>$_GET['id']]);
-    $array = $r->fetchAll(PDO::FETCH_ASSOC);
-    header('location: main.php');
-?>
+
